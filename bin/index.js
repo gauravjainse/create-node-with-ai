580abe15af403bb76159c0e4affb858793bc7825
gauravjainse <gauravjainse@gmail.com>
2025-09-04 16:52:33 +0530

    (feature/copy-template): add copy template directory feature

--- bin/index.js
+++ bin/index.js
@@ -1,10 +1,57 @@
 import chalk from "chalk";
 import { cliPrompts } from "../src/prompts.js";
+import path from "path";
+import fs, { readdirSync } from "fs";
+import { execSync } from "child_process";
+import { fileURLToPath } from "url";
+import { copyDir } from "../src/utils/copyDir.js";
+import { renderTokens } from "../src/utils/render.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 async function main() {
-  const options = await cliPrompts();
+  try {
+    const answers = await cliPrompts();
 
-  console.log(`${chalk.greenBright("Welcome to create-node-with-ai package")}`);
+    const { projectName, projectType, dependencyInstall, projectLang } =
+      answers;
+
+    const templateRoot = path.join(
+      __dirname,
+      "..",
+      "src",
+      "templates",
+      projectType,
+      projectLang,
+      "project"
+    );
+
+    if (!fs.existsSync(templateRoot)) {
+      console.error("Template not found:", templateRoot);
+      process.exit(1);
+    }
+
+    const targetedDir = path.resolve(process.cwd(), projectName);
+
+    if (fs.existsSync(targetedDir) && readdirSync(targetedDir).length > 0) {
+      console.log(`Targeted directory ${projectName} is not empty.`);
+      process.exit(1);
+    }
+
+    copyDir(templateRoot, targetedDir);
+
+    answers.install
+      ? console.log(`${chalk.greenBright("⚙️ Installing dependencies..")}`)
+      : process.exit(1);
+
+    console.log(
+      `${chalk.greenBright("🚀 setting up the project, please wait..")}`
+    );
+  } catch (error) {
+    console.log("Error", error.message);
+    process.exit(1);
+  }
 }
 
 main();
